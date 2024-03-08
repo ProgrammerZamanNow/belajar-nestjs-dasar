22198c128d75baf2de57e33761c0626c9afc5aec
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2024-03-08 23:48:17 +0700

    Add sample API response and redirect methods to UserController
    The UserController has been extended by adding two new methods: "sampleResponse", which returns a simple JSON response, and "redirect", which redirects requests to the sample response URI. Necessary decorators and imports from '@nestjs/common' were also added.

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,7 +1,35 @@
-import { Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  HttpRedirectResponse,
+  Param,
+  Post,
+  Query,
+  Redirect,
+} from '@nestjs/common';
 
 @Controller('/api/users')
 export class UserController {
+  @Get('/sample-response')
+  @Header('Content-Type', 'application/json')
+  @HttpCode(200)
+  sampleResponse(): Record<string, string> {
+    return {
+      data: 'Hello JSON',
+    };
+  }
+
+  @Get('/redirect')
+  @Redirect()
+  redirect(): HttpRedirectResponse {
+    return {
+      url: '/api/users/sample-response',
+      statusCode: 301,
+    };
+  }
+
   @Get('/hello')
   sayHello(
     @Query('first_name') firstName: string,
