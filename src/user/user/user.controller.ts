b9e8fc6ca47099d86a1e31a01e617dc071836895
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2024-03-08 23:14:12 +0700

    Add GET methods to UserController for hello and id
    This commit adds two GET methods to UserController. The first GET method, sayHello(), takes 'first_name' and 'last_name' queries and returns a greeting string. The second getByID() method fetches user data by 'id'. These additions provide users with personalized greeting and

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,7 +1,21 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('/api/users')
 export class UserController {
+  @Get('/hello')
+  sayHello(
+    @Query('first_name') firstName: string,
+    @Query('last_name') lastName: string,
+  ): string {
+    return `Hello ${firstName} ${lastName}`;
+  }
+
+  @Get('/:id')
+  getById(@Param('id') id: string): string {
+    return `GET ${id}`;
+  }
+
   @Post()
   post(): string {
     return 'POST';
