d4f1d2b6833397a324de826e0f0952ae17117d02
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2024-03-08 21:29:12 +0700

    Add POST and GET methods to UserController
    This commit introduces POST and GET methods to UserController. The POST method is implemented to return a 'POST' string, while the GET method handles requests to '/sample' path and returns a 'Hello NestJS' string. This enhances the functionality of handling user-related requests.

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('/api/users')
-export class UserController {}
+export class UserController {
+  @Post()
+  post(): string {
+    return 'POST';
+  }
+
+  @Get('/sample')
+  get(): string {
+    return 'Hello NestJS';
+  }
+}
