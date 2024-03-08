cf52c67bfc3e76542efbbfa8f863d4d70443720b
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2024-03-09 00:18:40 +0700

    Add node-mocks-http package and update tests
    The node-mocks-http package has been added to the project's dependencies. This enables simulation of HTTP behavior for more effective testing. User controller tests have also been updated to cover more functionalities, such as the 'sayHello' method and template rendering.

--- src/user/user/user.controller.spec.ts
+++ src/user/user/user.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import * as httpMock from 'node-mocks-http';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -7,12 +8,26 @@ describe('UserController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      imports: [],
+      providers: [],
     }).compile();
 
     controller = module.get<UserController>(UserController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should can say hello', async () => {
+    const response = await controller.sayHello('Eko', 'Khannedy');
+    expect(response).toBe('Hello Eko Khannedy');
+  });
+
+  it('should can view template', async () => {
+    const response = httpMock.createResponse();
+    controller.viewHello('Eko', response);
+
+    expect(response._getRenderView()).toBe('index.html');
+    expect(response._getRenderData()).toEqual({
+      name: 'Eko',
+      title: 'Template Engine',
+    });
   });
 });
